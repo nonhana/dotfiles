7637a36b721f89486605460e18d8ad2fcec69248
non_hana <nonhana@outlook.com>
2026-07-29 23:05:54 +0800

    init

---
+++ private_dot_local/share/private_fish/secrets.fish
@@ -0,0 +1,17 @@
+set -x ANTHROPIC_AUTH_TOKEN sk-rc-ai-a9UC4di7oM9KMAp5jK7dQw4wUyu45v3AvpByvqy2G7s
+set -x ANTHROPIC_VERTEX_BASE_URL https://gateway.rightcapital.ai/api
+set -x ANTHROPIC_VERTEX_PROJECT_ID rightcapital
+set -x CLAUDE_CODE_SKIP_VERTEX_AUTH 1
+set -x CLAUDE_CODE_USE_VERTEX 1
+set -x CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC 1
+
+# RightCapital Codex API Key
+set -x OPENAI_RIGHT_API_KEY sk-rc-ai-a9UC4di7oM9KMAp5jK7dQw4wUyu45v3AvpByvqy2G7s
+
+# Anyone AI API Key
+set -x OPENAI_ANYONE_API_KEY sk-DyKdslYqwOflOaEUugumBvuiFUo8OjxBobiACbYHSXAPAqsz
+
+# PackyAPI Codex API Key
+set -x OPENAI_PACKY_API_KEY sk-Tdvk3lyQSv2tcFJyppMriAfFJiBFlpY9x3wguGWah5YATPHG
+
+set -x RIGHTCAPITAL_AI_KEY sk-rc-ai-a9UC4di7oM9KMAp5jK7dQw4wUyu45v3AvpByvqy2G7s
